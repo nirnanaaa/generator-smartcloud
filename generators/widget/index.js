c4e176a44ef2de384b0c71153c1be80a1cf6a725
nirnanaaa <mosny@zyg.li>
2017-09-29 09:18:54 +0200

    add more core

--- generators/widget/index.js
+++ generators/widget/index.js
@@ -1,36 +1,50 @@
-'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const util = require('../../util');
+const mkdirp = require('mkdirp');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the world-class ' + chalk.red('generator-widget') + ' generator!'
-    ));
+  constructor(args, opts) {
+    super(args, opts);
 
+    this.option('skip-install-message', {
+      desc: 'Skips the message after the installation of dependencies',
+      type: Boolean
+    });
+
+    this.option('test-framework', {
+      desc: 'Test framework to be invoked',
+      type: String,
+      defaults: 'mocha'
+    });
+
+    this.option('babel', {
+      desc: 'Use Babel',
+      type: Boolean,
+      defaults: true
+    });
+  }
+
+  prompting() {
     const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
+      type    : 'input',
+      name    : 'name',
+      message : 'Component Name',
+      default : 'SampleWidget' // Default to current folder name
     }];
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+    return this.prompt(prompts).then(answers => {
+      this.name = answers.name;
     });
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('public/index.html'),
+      { title: 'Templating with Yeoman' }
     );
   }
 
-  install() {
-    this.installDependencies();
-  }
 };
